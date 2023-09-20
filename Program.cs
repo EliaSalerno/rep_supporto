8ab78fea5f7c6d625e58964aca24e2953d6a24e9
EliaSalerno <elia.salerno@issgreppi.it>
2023-09-20 10:50:38 +0200

    Master: aggiunta cs prova

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string message="Hello world!";
+			Console.WriteLine(message);
+		}
+	}
+}
