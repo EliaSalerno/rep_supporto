209968643f0da4d1280d51dd4e817c1873d57439
EliaSalerno <elia.salerno@issgreppi.it>
2023-09-25 06:57:10 +0000

    Master: aggiunta mini progetto cs

---
+++ progettocs/progetto.cs
@@ -0,0 +1,16 @@
+using System;
+using Syste.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Program
+    {
+    	static void main(string[] args)
+    	{
+    		Console.Write("Benvenuto");
+    	}
+    }    
+}
