4573ede24488dae5184ad7db6860ca719083656d
EliaSalerno <elia.salerno@issgreppi.it>
2023-10-09 09:23:50 +0200

    Main: completamento con compilazione di ese museo

---
+++ Linq/ese_museo/Artista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ese_museo
+{
+	public class Artista
+	{
+		public int Id {  get; set; }
+		public string Nome { get; set; } = null!;
+		public string Cognome { get; set; }=null!;
+		public string? Nazionalita {  get; set; }
+		public override string ToString()
+		{
+			return string.Format($"[ID={Id},Nome={Nome},Cognome={Cognome},Nazionalità={Nazionalita}]");
+		}
+	}
+}
---
+++ Linq/ese_museo/Personaggio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ese_museo
+{
+    public class Personaggio
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public int FkOperaId { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"[ID = {Id}, Nome = {Nome}, FkOperaId = {FkOperaId}]"); ;
+        }
+    }
+}
